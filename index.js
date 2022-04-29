ab85638ca3bd9f07eb226c3e948342c4156d45cf
Michael Wang <mikehjwang@gmail.com>
2022-04-29 08:44:47 -0700

    implement readline for CLI input

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+const readline = require('readline')
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
 const calc = (firstTerm, secondTerm, opt) => {
   if (opt === '+') {
     return firstTerm + secondTerm
